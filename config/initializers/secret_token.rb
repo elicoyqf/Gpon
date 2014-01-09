b22a979246bd7930dd32f4689b10b6d8bf9a01cf
elico <elico_yqf@sina.com>
2014-01-09 23:48:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gpon::Application.config.secret_key_base = '46b7dad4c04706a62dabfefa8543a579355bdb8d1373b8ad75cbb70dbab5144836fa71b0ffe68dbb4ff95ad871890eb3469c65336aaf52a5c7d2107cfd607462'
